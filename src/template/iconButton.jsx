acab415ed989c3acec2899b5f7a05c4b7d83b36b
João Souza <j.otavioms@gmail.com>
2020-01-08 20:51:34 -0300

    Add If component helper

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import If from './if';
+
 
 export default props => {
-  if (props.hide) return null;
-
   return (
-    <button className={`btn btn-${props.style}`} onClick={props.onClick}>
-      <i className={`fa fa-${props.icon}`}></i>
-    </button>
+    <If test={!props.hide}>
+      <button className={`btn btn-${props.style}`} onClick={props.onClick}>
+        <i className={`fa fa-${props.icon}`}></i>
+      </button>
+    </If>
   )
 }
