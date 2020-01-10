f9710e41956341bb09217b13c7463b1362ea8966
João Souza <j.otavioms@gmail.com>
2020-01-10 14:09:24 -0300

    Add handle search

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -15,15 +15,18 @@ export default class Todo extends Component {
     this.handleAdd = this.handleAdd.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleRemove = this.handleRemove.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
     this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
     this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
 
     this.refresh();
   };
 
-  refresh() {
-    Axios.get(`${URL}?sort=-createdAt`)
-      .then(resp => this.setState({ ...this.state, description: '', list: resp.data }));
+  refresh(description = '') {
+    const search = description ? `&description__regex=/${description}/` : '';
+
+    Axios.get(`${URL}?sort=-createdAt${search}`)
+      .then(resp => this.setState({ ...this.state, description, list: resp.data }));
   };
 
   handleAdd() {
@@ -39,17 +42,21 @@ export default class Todo extends Component {
 
   handleRemove(todo) {
     Axios.delete(`${URL}/${todo._id}`)
-      .then(resp => this.refresh());
+      .then(resp => this.refresh(this.state.description));
+  };
+
+  handleSearch() {
+    this.refresh(this.state.description);
   };
 
   handleMarkAsDone(todo) {
     Axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
-      .then(resp => this.refresh());
+      .then(resp => this.refresh(this.state.description));
   };
 
   handleMarkAsPending(todo) {
     Axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
-      .then(resp => this.refresh());
+      .then(resp => this.refresh(this.state.description));
   };
 
   render() {
@@ -60,6 +67,7 @@ export default class Todo extends Component {
           description={this.state.description} 
           handleAdd={this.handleAdd} 
           handleChange={this.handleChange} 
+          handleSearch={this.handleSearch}
         />
         <TodoList 
           list={this.state.list} 
